a3eeef80134995193b2715fb7404bd5080aed78f
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-07-31 09:43:34 -0600

    thanks josh

--- database-connection.js
+++ database-connection.js
@@ -1,3 +1,18 @@
-const CONFIG = require('./knexfile')[process.env.NODE_ENV || 'development']
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = 9000
+const cors = require('cors')
 
-module.exports = require('knex')(CONFIG)
+app.use(cors())
+app.use(bodyParser.json())
+
+
+app.get('/',(request,response,next) => {
+  response.send('🎉')
+})
+
+
+app.listen(port,() => {
+  console.log(`I am listening on ${port}`)
+})
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const app = require('./app')
 
 app.listen(process.env.PORT || 3000)
+
+var pg = require('knex')({
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING,
+  searchPath: ['knex', 'public'],
+})
